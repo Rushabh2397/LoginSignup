560b9110bf8d0da3a1c741b174cf34b86534d36a
Rushabh2397 <rushabhsancheti2397@gmail.com>
2021-01-02 13:16:36 +0530

    refactored the code

--- myscript.js
+++ myscript.js
@@ -1,11 +1,20 @@
-const sliderLeftBtn= document.querySelector("#btn-signin")
-const sliderRightBtn=document.querySelector("btn-signup");
-const leftPanel=document.querySelector("left-panel");
-const rightPanel=document.querySelector("right-panel");
-const slidingPanelLeft=document.querySelector(".sliding-panel-left")
+const sliderLeftBtn = document.querySelector("#btn-signin")
+const sliderRightBtn = document.querySelector("#btn-signup");
+const leftPanel = document.querySelector("left-panel");
+const rightPanel = document.querySelector("right-panel");
+const slidingPanelLeft = document.querySelector(".sliding-panel-left")
+const slidingPanelRight = document.querySelector(".sliding-panel-right")
 
 
- sliderLeftBtn.addEventListener("click",()=>{
-    slidingPanelLeft.style.display="none";
-    console.log("rushabh")
- })
+
+slidingPanelRight.style.display = "none";
+
+sliderLeftBtn.addEventListener("click", () => {
+   slidingPanelLeft.style.display = "none";
+   slidingPanelRight.style.display = "flex";
+})
+
+sliderRightBtn.addEventListener("click",()=>{
+   slidingPanelLeft.style.display = "flex";
+   slidingPanelRight.style.display = "none";
+})
