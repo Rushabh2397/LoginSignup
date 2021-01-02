40d979e2a54686772ea4eb6d24481936ace31afd
Rushabh2397 <rushabhsancheti2397@gmail.com>
2021-01-02 12:06:52 +0530

    intial commit

---
+++ myscript.js
@@ -0,0 +1,11 @@
+const sliderLeftBtn= document.querySelector("#btn-signin")
+const sliderRightBtn=document.querySelector("btn-signup");
+const leftPanel=document.querySelector("left-panel");
+const rightPanel=document.querySelector("right-panel");
+const slidingPanelLeft=document.querySelector(".sliding-panel-left")
+
+
+ sliderLeftBtn.addEventListener("click",()=>{
+    slidingPanelLeft.style.display="none";
+    console.log("rushabh")
+ })
